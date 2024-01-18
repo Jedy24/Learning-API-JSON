96f4a45ea337b8391eaefe165fa45124d9b1fa97
Jedy24 <reihimawari27@gmail.com>
2024-01-18 13:05:52 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\FacebookController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -36,3 +38,23 @@ Route::post('logout-facebook', [FacebookController::class, 'logout'])
     ->name('logout-facebook');
 
 Route::get('/json', [HomeController::class, 'indexJson'])->name('json-data');
+
+// Registrasi dan Login Manual
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])
+    ->middleware(['guest'])
+    ->name('show-registration-form');
+
+Route::post('register', [RegisterController::class, 'register'])
+    ->middleware(['guest'])
+    ->name('register');
+
+Route::get('login', [LoginController::class, 'showLoginForm'])
+    ->middleware(['guest'])
+    ->name('login');
+
+Route::post('login', [LoginController::class, 'login'])
+    ->middleware(['guest']);
+
+Route::post('logout', [LoginController::class, 'logout'])
+    ->middleware(['auth'])
+    ->name('logout');
